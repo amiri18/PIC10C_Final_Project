6d887cb1a1ab5c28b6fcb62a8bc0d088bea62f47
Amiri Irish <amiri.irish13@gmail.com>
2018-05-29 00:32:51 -0700

    Code runs! Haven't tested every function, but the game works, where if you get to the other side the piece will be "queened". also you can "kill" your opponent. Need to work on the error messages and double jumping. Then will code the AI.

--- PIC10CFinalProject/PIC10CFinalProject/CheckersGame.cpp
+++ PIC10CFinalProject/PIC10CFinalProject/CheckersGame.cpp
@@ -2,7 +2,63 @@
 #include <iostream>
 using namespace std;
 
-CheckBoard::CheckBoard() : blackCount(12), whiteCount(12) {
+Player::Player(int sym, int roy, const CheckBoard& check) : pieceCount(12), symbol(sym), royal(roy), royalCount(0), startRow(0) {
+    if (sym == 8){
+        startRow = 7;
+    }
+    else {
+        startRow = 0;
+    }
+    for (int i = 0; i < 8; ++i){
+        for (int j = 0; j < 8; ++j){
+            if (check.taken(i, j, getSym())){
+                pair<int, int> p = make_pair(i, j);
+                location.push_back(p);
+            }
+        }
+    }
+    
+}
+
+int Player::avaliableSpots(const CheckBoard& board) const{
+    int count = 0;
+    for (int i = 0; i < 8; ++i){
+        for (int j = 0; j < 8; ++j){
+            if (board.taken(i, j, 1)){
+                ++count;
+            };
+        }
+    }
+    return count;
+}
+
+int Player::getSym() const {
+    return symbol;
+}
+
+int Player::getRoy() const {
+    return royal;
+}
+
+void Player::Move(int Crow, int Ccol, int Nrow, int Ncol) {
+    for (int i = 0; i < location.size(); ++i){
+        if (location[i].first == Crow && location[i].second == Ccol){
+            location[i].first = Nrow;
+            location[i].second = Ncol;
+        }
+    }
+}
+
+void Player::ate(const pair<int,int>& coord){
+    for (int i = 0; i < location.size(); ++i){
+        if (location[i] == coord){
+            location.erase(location.begin()+i);
+            
+        }
+    }
+}
+
+CheckBoard::CheckBoard() : nineCount(12), eightCount(12) {
     vector<int> row1 = {0,9,0,9,0,9,0,9};
     vector<int> row2 = {9,0,9,0,9,0,9,0};
     vector<int> row3 = {0,9,0,9,0,9,0,9};
@@ -25,31 +81,107 @@ char CheckBoard::dis(int input) const{
     switch (input) {
         case 0:
             return ' ';
-            break;
         case 1:
             return ' ';
-            break;
         case 8:
             return 'O';
-            break;
         case 9:
-            return '@';
-            break;
+            return 'X';
+        case 7:
+            return 'K';
+        case 6:
+            return 'Q';
         default:
             return ' ';
-            break;
     }
 }
 
 void CheckBoard::display() const{
+    cout << "    0   1   2   3   4   5   6   7\n";
     for (size_t i = 0; i < board.size(); ++i){
-        cout << "+---+---+---+---+---+---+---+---+\n";
+        cout << "  +---+---+---+---+---+---+---+---+\n";
+        cout << i << " ";
         for (size_t j = 0; j < board.size(); ++j){
             cout << "| " << dis(board[i][j]) << " ";
         }
         cout << "|\n";
     }
-    cout << "+---+---+---+---+---+---+---+---+\n";
+    cout << "  +---+---+---+---+---+---+---+---+\n";
+}
+
+bool CheckBoard::taken(int row, int col, int sym) const{
+    return board[row][col] == sym;
+}
+
+void CheckBoard::didKill(int Crow, int Ccol, int Nrow, int Ncol, Player& player){
+    if (!((Nrow-Crow) == 1 || Nrow-Crow == -1)){
+        grave(Crow, Ccol, Nrow, Ncol, player);
+    }
+}
+
+void CheckBoard::move(int Crow, int Ccol, int Nrow, int Ncol, Player& player){
+    int sym = board[Crow][Ccol];
+    
+    if (sym == 8 && Nrow == 0){
+        sym = 6;
+        ++player.royalCount;
+    }
+    if (sym == 9 && Nrow == 7){
+        sym = 7;
+        ++player.royalCount;
+    }
     
     
+    if (board[Crow][Ccol] == player.getSym() || board[Crow][Ccol] == player.getRoy()){
+        board[Crow][Ccol] = 1;
+    }
+    if (board[Nrow][Ncol] == 1){
+        board[Nrow][Ncol] = sym;
+    }
+    player.Move(Crow, Ccol, Nrow, Ncol);
+}
+
+void CheckBoard::RIP(int row, int col, Player& player){
+    int sym = board[row][col];
+    cout << "RIP spot: (" << row << "," << col << ")\n";
+    board[row][col] = 1;
+    pair<int, int> p = make_pair(row, col);
+    switch (sym) {
+        case 8:
+            --eightCount;
+            player.ate(p);
+            break;
+        case 9:
+            --nineCount;
+            player.ate(p);
+            break;
+        case 6:
+            --eightCount;
+            --player.royalCount;
+            player.ate(p);
+            break;
+        case 7:
+            --nineCount;
+            --player.royalCount;
+            player.ate(p);
+            break;
+    }
+}
+
+void CheckBoard::grave(int Crow, int Ccol, int Nrow, int Ncol, Player& player){
+    int RIProw = 0;
+    int RIPcol = 0;
+    if ((Nrow - Crow) >= 0){ // moved straight
+        RIProw = Crow + 1;
+    }
+    if ((Nrow - Crow) < 0){ // moved backward
+        RIProw = Crow - 1;
+    }
+    if ((Ncol - Ccol) >= 0) { // moved right
+        RIPcol = Ccol + 1;
+    }
+    if ((Ncol - Ccol) < 0) { // moved left
+        RIPcol = Ccol - 1;
+    }
+    RIP(RIProw, RIPcol, player);
 }
--- PIC10CFinalProject/PIC10CFinalProject/main.cpp
+++ PIC10CFinalProject/PIC10CFinalProject/main.cpp
@@ -5,27 +5,57 @@ using namespace std;
 
 int main() {
     CheckBoard board;
+    Player playerO(8, 6, board);
+    Player playerX(9, 7, board);
+    bool keepGoing = true;
     board.display();
+    size_t count = 0;
+    while (keepGoing){
+        if (count%2 == 1){
+            int Crow = 0;
+            int Ccol = 0;
+            int Nrow = 0;
+            int Ncol = 0;
+            cout << "Where do you want to move Player O?\nCurrent Row and Current Column: ";
+            cin >> Crow >> Ccol;
+            cout << "New Row and New Col: ";
+            cin >> Nrow >> Ncol;
+            board.move(Crow, Ccol, Nrow, Ncol, playerO);
+            board.didKill(Crow, Ccol, Nrow, Ncol, playerX);
+            board.display();
+            char quit = 'N';
+            cout << "To quit, enter 'Q' or press any key to continue ";
+            cin >> quit;
+            if (quit == 'Q'){
+                keepGoing = false;
+            }
+            ++count;
+        }
+        else {
+            int Crow = 0;
+            int Ccol = 0;
+            int Nrow = 0;
+            int Ncol = 0;
+            cout << "Where do you want to move Player X?\nCurrent Row and Current Column: ";
+            cin >> Crow >> Ccol;
+            cout << "New Row and New Col: ";
+            cin >> Nrow >> Ncol;
+            board.move(Crow, Ccol, Nrow, Ncol, playerX);
+            board.didKill(Crow, Ccol, Nrow, Ncol, playerO);
+            board.display();
+            char quit = 'N';
+            cout << "To quit, enter 'Q' or press any key to continue ";
+            cin >> quit;
+            if (quit == 'Q'){
+                keepGoing = false;
+            }
+            ++count;
+        }
+        
+    }
+    
+
     return 0;
 }
 
-/*
- CheckerBoard Display *for now*
-+---+---+---+---+---+---+---+---+
-|   | @ |   | @ |   | @ |   | @ |
-+---+---+---+---+---+---+---+---+
-| @ |   | @ |   | @ |   | @ |   |
-+---+---+---+---+---+---+---+---+
-|   | @ |   | @ |   | @ |   | @ |
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |   |   |   |
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |   |   |   |
-+---+---+---+---+---+---+---+---+
-| O |   | O |   | O |   | O |   |
-+---+---+---+---+---+---+---+---+
-|   | O |   | O |   | O |   | O |
-+---+---+---+---+---+---+---+---+
-| O |   | O |   | O |   | O |   |
-+---+---+---+---+---+---+---+---+
-*/
+
